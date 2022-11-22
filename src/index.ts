af75b92d8b7cdcddf427b07b18cb95f04ab1b682
Tom Elliott <tmelliottjr@github.com>
2022-11-22 10:49:18 -0500

    handle identifier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Octokit } from "@octokit/core";
 import { VERSION } from "./version";
 import { Endpoints } from "@octokit/types";
 
-const OCTOKIT_UNIQUE_ISSUE_ID_PREFIX = "OCTOKIT_UNIQUE_ISSUE_ID:";
+const OCTOKIT_UNIQUE_ISSUE_ID_PREFIX = "octokit-unique-issue id=";
 
 type CreateOrUpdateUniqueIssueOptionsT =
   Endpoints["POST /repos/{owner}/{repo}/issues"]["parameters"] & {
@@ -14,10 +14,6 @@ type CreateOrUpdateUniqueIssueResponseT =
   | Endpoints["POST /repos/{owner}/{repo}/issues"]["response"]
   | Endpoints["PATCH /repos/{owner}/{repo}/issues/{issue_number}"]["response"];
 
-/**
- * @param octokit Octokit instance
- * @param options Options passed to Octokit constructor
- */
 export function uniqueIssue(octokit: Octokit) {
   return {
     async createOrUpdateUniqueIssue(
@@ -32,32 +28,23 @@ export function uniqueIssue(octokit: Octokit) {
         ...rest
       } = options;
 
+      if (!identifier) {
+        throw Object.assign(new Error("identifier is required."), {
+          name: "MissingIdentifierError",
+        });
+      }
+
       const term = OCTOKIT_UNIQUE_ISSUE_ID_PREFIX + identifier;
 
+      // MDAST compatible comment marker; see https://github.com/syntax-tree/mdast-comment-marker
       const commentMarker = `<!-- ${term} -->`;
 
       const {
         data: { items, total_count },
       } = await octokit.request("GET /search/issues", {
-        q: `${term} is:issue is:open repo:${owner}/${repo}`,
+        q: `"${term}" is:issue is:open repo:${owner}/${repo}`,
       });
 
-      // If close_previous is set, close any existing issues found with unique key
-      if (total_count !== 0 && !update_previous) {
-        for (const item of items) {
-          await octokit.request(
-            "PATCH /repos/{owner}/{repo}/issues/{issue_number}",
-            {
-              owner,
-              repo,
-              issue_number: item.number,
-              state: "closed",
-            }
-          );
-        }
-      }
-
-      // If one and only one issue was found, update!
       if (total_count === 1 && update_previous) {
         return await octokit.request(
           "PATCH /repos/{owner}/{repo}/issues/{issue_number}",
@@ -73,19 +60,29 @@ export function uniqueIssue(octokit: Octokit) {
         );
       }
 
-      // If more than one issue was found, throw!
-      if (total_count > 1) {
+      if (total_count > 1 && update_previous) {
         throw Object.assign(
           new Error("More than 1 issue was found with identifier."),
           {
             identifier,
             issue_numbers: items.map((item) => item.number).join(),
-            name: "DuplicateUniqueIssueError",
+            name: "DuplicateIssueError",
           }
         );
       }
 
-      // Create
+      for (const item of items) {
+        await octokit.request(
+          "PATCH /repos/{owner}/{repo}/issues/{issue_number}",
+          {
+            owner,
+            repo,
+            issue_number: item.number,
+            state: "closed",
+          }
+        );
+      }
+
       return await octokit.request("POST /repos/{owner}/{repo}/issues", {
         owner,
         repo,
